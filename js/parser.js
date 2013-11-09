ac7a970b81bae0c63234abb5eebba1e5a3f8f302
MaybeNot <gaboureystockton@gmail.com>
2013-11-09 17:06:27 -0500

    finished parser. bout time

--- js/parser.js
+++ js/parser.js
@@ -13,73 +13,84 @@ console.log("parser.js loaded");
 	};
 
 	var getValidTags = function (field) {
-	var text = field.value;
-	var pattern = /{{([^{}\s]*?)}}/;
+		parse(field.value, field);
+	};
 
-	var match = null;
-	var count = 0;
-	while((match = pattern.exec(text)) !== null) {
-		//console.log("Match at index " + match.index);
-		//console.log("\tMatch: " + match[1]);
+	var parse = function(text, field) {
+		var pattern = /{{([^{}\s]*?)}}/;
 
-		//check match subpatterns
-		// {{#label}}
-		// {{&label}}
-		// {{label#(N)}}
-		var giphy = false;
-		var stackPattern = /#(.*)$/g;
-		var randomPattern = /&(.*)$/g;
-		var numPattern = /([^#].*)#([0-9]+)/g;
-		var subMatch = null;
-		var handledText = "";
+		var match = null;
+		var count = 0;
 
-		var label = match[1];
-		var gifIndex = 0;
+		if((match = pattern.exec(text)) !== null) {
+			//console.log("Match at index " + match.index);
+			//console.log("\tMatch: " + match[1]);
 
-		if(subMatch = numPattern.exec(match[1])) {
-			//console.log("num pattern: " + subMatch[1] + " number " + subMatch[2]);
-			handledText = "[" + subMatch[1] + " number " + subMatch[2] + "]";
-			label = subMatch[1];
-			gifIndex = parseInt(subMatch[2]) - 1;
+			//check match subpatterns
+			// {{#label}}
+			// {{&label}}
+			// {{label#(N)}}
+			var stackPattern = /#(.*)$/g;
+			var randomPattern = /&(.*)$/g;
+			var numPattern = /([^#].*)#([0-9]+)/g;
+			var subMatch = null;
+			var handledText = "";
+
+			var label = match[1];
+			var gifIndex = 0;
+
+			if(subMatch = numPattern.exec(match[1])) {
+				//console.log("num pattern: " + subMatch[1] + " number " + subMatch[2]);
+				//handledText = "[" + subMatch[1] + " number " + subMatch[2] + "]";
+				label = subMatch[1];
+				gifIndex = parseInt(subMatch[2]) - 1;
+				setHandledText(gifIndex, match[1], label, data, text, field);
+			}
+			else if(subMatch = stackPattern.exec(match[1])) {
+				//console.log("stack pattern: " + subMatch[1]);
+				//handledText = "[Smart stack " + subMatch[1] + "]";
+				//giphy url with label
+				$.get("http://hierogifics.herokuapp.com/" + subMatch[1], function(data) {
+					var json = JSON.parse(data);
+					var gifIndex = Math.floor(Math.random()*json.data.length);
+					var url = json.data[gifIndex].url;
+					setHandledGiphyText(match[1], url, text, field);
+				});
+			}
+			else if(subMatch = randomPattern.exec(match[1])) {
+				//console.log("random pattern: " + subMatch[1]);
+				//handledText = "[Random from " + subMatch[1] + "]";
+				label = subMatch[1];
+				if(categoryIndexOf(label) !== -1)
+					gifIndex = Math.floor(Math.random()*data.categories[categoryIndexOf(label)].gifs.length);
+				setHandledText(gifIndex, match[1], label, data, text, field);
+			}
+			else {
+				setHandledText(0, match[1], match[1], data, text, field)
+			}
+
+			//console.log("remaining: " + text);
+			//process match
 		}
-		else if(subMatch = stackPattern.exec(match[1])) {
-			//console.log("stack pattern: " + subMatch[1]);
-			handledText = "[Smart stack " + subMatch[1] + "]";
-			//giphy url with label
-			var json = $.get("http://hierogifics.herokuapp.com/" + subMatch[1]);
-			console.log(subMatch[1]);
-			console.log(json);
-			var index = Math.floor(Math.random()*json.data.length);
-			handledText = json.data[index].url;
-			giphy = true;
+	};
 
-		}
-		else if(subMatch = randomPattern.exec(match[1])) {
-			//console.log("random pattern: " + subMatch[1]);
-			handledText = "[Random from " + subMatch[1] + "]";
-			label = subMatch[1];
-			if(categoryIndexOf(label) !== -1)
-				gifIndex = Math.floor(Math.random()*data.categories[categoryIndexOf(label)].gifs.length);
-		}
+	var setHandledGiphyText = function(label, url, text, field) {
+		text = text.replace("{{" + label + "}}", url);
+		field.value = text;
+		parse(text, field);
+	};
 
-		if(!giphy) {
-			var index = -1;
+	var setHandledText = function(gifIndex, matched, label, collection, text, field) {
+		var index = -1;
 			//console.log(label + "[" + gifIndex + "]");
 			if((index = categoryIndexOf(label)) !== -1)
-				handledText = data.categories[index].gifs[gifIndex%data.categories[index].gifs.length].animated;
+				handledText = collection.categories[index].gifs[gifIndex%collection.categories[index].gifs.length].animated;
 			else
 				handledText = "[invalid category]";
-		}
 
-		text = text.replace("{{" + match[1] + "}}", handledText);
-		//console.log("remaining: " + text);
-		//process match
-	}
-	//console.log("Matching complete");
-
-	field.value = text;
-
-	return text;
+		text = text.replace("{{" + matched + "}}", handledText);
+		field.value = text;
+		parse(text, field);
 	};
 
 	var categoryIndexOf = function(label) {
