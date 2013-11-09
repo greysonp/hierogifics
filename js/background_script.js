05261798be81c23bac4072f83dfa516e42247af5
Greyson Parrelli <greyson.parrelli@live.com>
2013-11-09 09:47:28 -0500

    Can add images via right click.
    Also got rough draft of putting those gifs in the toolbar.

---
+++ js/background_script.js
@@ -0,0 +1,117 @@
+// Create one test item for each context type.
+var contexts = ["link","editable","image"];
+
+// Create a context menu for an image
+chrome.storage.sync.get("categories", function(categoriesObj) {
+    if (!categoriesObj || Object.keys(categoriesObj).length === 0) {
+        initCategories(function(categoriesObj) {
+            initContextMenu(categoriesObj.categories);
+        });
+    }
+    else {
+        initContextMenu(categoriesObj.categories);
+    }
+});
+
+function initContextMenu(categories) {
+    var imageParent = chrome.contextMenus.create({"title": "Save Image in Category", "contexts": ["image"]});
+    for (var i = 0; i < categories.length; i++) {
+        
+        // Create all of the children for the image context menu
+        (function(category){
+            chrome.contextMenus.create({
+                "title": categories[i].name, 
+                "parentId": imageParent,
+                "contexts": ["image"], 
+                "onclick": function(info, tab) {
+                    imageOnClick(info, tab, category);
+                }
+            })    
+        })(categories[i].name);
+    }
+}
+
+function imageOnClick(info, tab, category) {
+    console.log(category + " | " + info.srcUrl);
+    chrome.storage.sync.get("categories", function(categories) {
+        console.log(categories);
+        if (!categories || Object.keys(categories).length == 0) {
+            initCategories(function(defaults) {
+                console.log(defaults);
+                saveToStorage(info.srcUrl, category, defaults);
+            });
+        }
+        else {
+            saveToStorage(info.srcUrl, category, categories);
+        }
+    });
+}
+
+function saveToStorage(url, category, categoriesObj) {
+    var storedCategories = categoriesObj.categories;
+    for (var i = 0; i < storedCategories.length; i++) {
+        var c = storedCategories[i];
+        if (c.name.toLowerCase() == category.toLowerCase()) {
+            c.gifs.push({"animated": url, "use_count": 0});
+            break;
+        }
+    }
+    chrome.storage.sync.set(categoriesObj);
+
+}
+
+function initCategories(callback) {
+    var categories = [{
+       name: "happy",
+       symbol: ":)",
+       gifs: [{
+            "animated": "http://media1.giphy.com/media/q6QHDGE3X4EWA/giphy.gif",
+            "use_count": 0
+        },
+        {
+            "animated": "http://media.giphy.com/media/dBu6VX8ZI9pSM/giphy.gif",
+            "use_count": 0
+        }]
+    },
+    {
+       name: "sad",
+       symbol: ":(",
+       gifs: [{
+            "animated": "http://media.giphy.com/media/XAcSy8eyMFpaU/giphy.gif",
+            "use_count": 0
+       },
+       {
+            "animated": "http://media.giphy.com/media/kB1IR4TzOoBnq/giphy.gif",
+            "use_count": 0
+       }]
+    },
+    {
+        name: "mad",
+        symbol: "D:<",
+        gifs: [{
+            "animated": "http://media2.giphy.com/media/vwu9UTwIwFr7G/giphy.gif",
+            "use_count": 0
+        },
+        {
+            "animated": "http://media1.giphy.com/media/zMe5y4Lfy52Sc/giphy.gif",
+            "use_count": 0
+        }]
+    }];
+    var obj = { "categories": categories };
+    chrome.storage.sync.set({ "categories": categories }, function() { callback(obj); });
+}
+
+
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     console.log(sender.tab ?
+//                 "from a content script:" + sender.tab.url :
+//                 "from the extension");
+//         if (request.categories) {
+//             sendResponse({"categories": ["dog", "hello"]});
+//         }
+//         return true;
+//   });
+
+
+
--- js/toolbelt.js
+++ js/toolbelt.js
@@ -1,13 +1,40 @@
 (function() {
     $.get(chrome.extension.getURL("html/toolbelt.html"), function(data) {
         $("body").append(data);
-        $("#js-next-button").click(nextCategory);
-        $("#js-belt ol li").mouseenter(setScrollToBottom);
-        $("#js-belt ol li").mouseleave(setScrollToBottom);
-        $("#js-belt ol li ol").css("-webkit-mask", "url(" + chrome.extension.getURL("img/toolbar_fade_mask.svg") + ")");
-        $("#js-belt ol li ol").bind("mousewheel", function(e) {
-            $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY / 2);
-            e.preventDefault();
+
+        chrome.storage.sync.get("categories", function(categoriesObj) {
+            var categories = categoriesObj.categories;
+
+            for (var i = 0; i < categories.length; i++) {
+                var name = categories[i].name;
+                var gifs = categories[i].gifs;
+                var li = document.createElement("li");
+                var ol = document.createElement("ol");
+
+                for (var j = 0; j < gifs.length; j++) {
+                    console.log(gifs[j]);
+                    var innerLi = document.createElement("li");
+                    var img = document.createElement("img");
+                    img.src = gifs[j].animated;
+                    innerLi.appendChild(img);
+                    ol.appendChild(innerLi);
+                    if (j == gifs.length - 1) {
+                        var img2 = document.createElement("img");
+                        img2.src = gifs[j].animated;
+                        li.appendChild(img2);
+                    }
+                }
+                li.appendChild(ol);
+                $("#js-belt ol")[0].appendChild(li);
+            }
+            $("#js-next-button").click(nextCategory);
+            $("#js-belt ol li").mouseenter(setScrollToBottom);
+            $("#js-belt ol li").mouseleave(setScrollToBottom);
+            $("#js-belt ol li ol").css("-webkit-mask", "url(" + chrome.extension.getURL("img/toolbar_fade_mask.svg") + ")");
+            $("#js-belt ol li ol").bind("mousewheel", function(e) {
+                $(this).scrollTop($(this).scrollTop() - e.originalEvent.wheelDeltaY / 2);
+                e.preventDefault();
+            });
         });
     });
 
