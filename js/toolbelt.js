1cad011c84d830e7ed33dc54a43e726087615d2b
Greyson Parrelli <greyson.parrelli@live.com>
2013-11-08 22:21:36 -0500

    Toolbelt slides up and down with cmd+shift+up.

--- js/toolbelt.js
+++ js/toolbelt.js
@@ -1,1 +1,25 @@
 console.log("toolbelt.js loaded");
+
+$.get(chrome.extension.getURL("html/toolbelt.html"), function(data) {
+    $("body").append(data);
+});
+
+Mousetrap.bind("command+shift+up", function(e) {
+    console.log("Hello");
+    showBelt();
+    return false;
+});
+
+Mousetrap.bind("command+shift+down", function(e) {
+    console.log("Goodbye");
+    hideBelt();
+    return false;
+});
+
+function showBelt() {
+    $("#js-toolbelt").animate({"bottom": "0"}, 250);
+}
+
+function hideBelt() {
+    $("#js-toolbelt").animate({"bottom": "-150px"}, 250);
+}
