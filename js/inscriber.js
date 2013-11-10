c6cbf5ac179c670306a64e14298f800b389929ef
MaybeNot <gaboureystockton@gmail.com>
2013-11-09 22:19:25 -0500

    inscriber works for img tags

--- js/inscriber.js
+++ js/inscriber.js
@@ -4,20 +4,24 @@ console.log("inscriber.js loaded");
 
 	//retrieve page information
 	var url = window.location.origin + window.location.pathname;
-	var json;
+	url = url.substring(document.location.protocol.length + 2);
+	//var pattern = /\//g;
+	//url = url.replace(pattern, ".");
 
+/*
+	console.log(encodeURIComponent(url));
 	$.get( //insert link
-		"http://hierogifics.herokuapp.com/db/read/hi", function(data) {
-        json = JSON.parse(data);
+		"http://hierogifics.herokuapp.com/db/read/" + encodeURIComponent(url), function(data) {
+        var json = JSON.parse(data);
 	    if(json){
 		    console.log(json);
-		    placeGIF();
+		    placeGIF(json);
 	    }
 	    else{
 		    console.log("DID NOT GET DATA BACK CORRECTLY");
 	    }
     });
-
+*/
         //Received valid input
 
 /*
@@ -40,97 +44,114 @@ console.log("inscriber.js loaded");
 
     */
 
-/*
-	var data =  {
+
+	var json =  {
     url: "http://en.wikipedia.org/wiki/Magnetism", // url of the page
     gifs: [
         {
-            id: '<img alt="" src="//upload.wikimedia.org/wikipedia/commons/thumb/9/93/Magnetic_quadrupole_moment.svg/240px-Magnetic_quadrupole_moment.svg.png" width="240" height="238" class="thumbimage" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/9/93/Magnetic_quadrupole_moment.svg/360px-Magnetic_quadrupole_moment.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/9/93/Magnetic_quadrupole_moment.svg/480px-Magnetic_quadrupole_moment.svg.png 2x">', // unique ID for html element
+            id: '//upload.wikimedia.org/wikipedia/commons/thumb/9/93/Magnetic_quadrupole_moment.svg/240px-Magnetic_quadrupole_moment.svg.png',
             user_id: 0, // user who uploaded the gif
             gif_url: "http://24.media.tumblr.com/200bdf0364f5482e3afbfe03f51b6f9d/tumblr_mghlfuodK01rjf826o1_400.gif", // url of where the gif is located (giphy, etc)
             category: "party" // Happy, angry, sad, etc
         },
         {
-            id: '<img alt="Solenoid" src="//upload.wikimedia.org/wikipedia/commons/thumb/0/0d/VFPt_Solenoid_correct2.svg/190px-VFPt_Solenoid_correct2.svg.png" width="190" height="78" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/0/0d/VFPt_Solenoid_correct2.svg/285px-VFPt_Solenoid_correct2.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/0/0d/VFPt_Solenoid_correct2.svg/380px-VFPt_Solenoid_correct2.svg.png 2x">', // unique ID for html element
+            id: '//upload.wikimedia.org/wikipedia/commons/thumb/0/0d/VFPt_Solenoid_correct2.svg/190px-VFPt_Solenoid_correct2.svg.png',
             user_id: 1, // user who uploaded the gif
             gif_url: "http://24.media.tumblr.com/200bdf0364f5482e3afbfe03f51b6f9d/tumblr_mghlfuodK01rjf826o1_400.gif", // url of where the gif is located (giphy, etc)
             category: "party" // Happy, angry, sad, etc
         }
-        ],
-    clues: [
-        {} // seems like a good place for it. Will figure it out later
-    ]
+        ]
+    //clues: [Replaced http://i.imgur.com/b3pgP1r.gif 
+        //{} // seems like a good place for it. Will figure it out later
+    //]
 };
-*/
-	var placeGIF = function() {
-		for(var i = 0; i < json.length; i++) {
+
+	var placeGIF = function(json) {
+		console.log(json.gifs.length);
+		for(var i = 0; i < json.gifs.length; i++) {
 			//find element
-			var element = jQuery.parseHTML(json[i].id);
-			var target = null;
-			if(element[0].localName.localeCompare("img") == 0) {
-				target = $("img[src='" + element[0].src.substring(5) + "']");
+			//var element = jQuery.parseHTML(json[i].id);
+			console.log("JSON: " + json.gifs[i])
+			
+			console.log("SEARCH FOR: " + json.gifs[i].id);
+
+			target = $("img[src='" + json.gifs[i].id + "']");
 				//Means the element to hide things under was found
+
+				console.log("MATCHING COUNT: " + target.length);
+
 				if(target.length != 0){
-					$(target).after("<img src='" + dajsonta[i].gif_url + "' class='hidden-hierogif'>");
+					$(target).after("<img src='" + json.gifs[i].gif_url + "' class='hidden-hierogif'>");
 					$(target).mouseover(function() {
 					$(this).next().removeClass("hidden-hierogif").addClass('revealed-hierogif');
-				});
+					});
 				}
-				//That element was not found on this page
+					//That element was not found on this page
 				else{
-					$.get("http://hierogifics.herokuapp.com/db/remove/", {gif_id: json[i].gif_id}, function(data) {
-                        json = JSON.parse(data);
-	                    if(json){
-		                    console.log(json);
-            	        }
-            	        else{
-		                    console.log("DID NOT GET DATA BACK CORRECTLY");
-	                    }
-                    });
-
+						/* Was not time to implement properly, will delete from the DB by hand. This would be the required request
+						$.get("http://hierogifics.herokuapp.com/db/remove/", {gif_id: json[i].gif_id}, function(data) {
+	                        json = JSON.parse(data);
+		                    if(json){
+			                    console.log(json);
+	            	        }
+	            	        else{
+			                    console.log("DID NOT GET DATA BACK CORRECTLY");
+		                    }
+	                    });
+						*/
 				}
-
-/*
-					$.get(
-					    //delete get url here
-					    "http://hierogifics.herokuapp.com/db/remove/gif/",
-					    {   type: "GET",
-					    //List of parameters to pass to the DB
-					        data: {gif_id: data.gifs[i].gif_id more query elements} ,
-					        dataType: "json",
-					        success: function(json) {
-					            //Dont have to do anything just deletes from the DB
-					        },
-					        error: function(jqXHR, textStatus, errorThrown) {
-					        	//Displays an error if the value was not deleted
-					            alert("Error: " + jqXHR.responseText);
-					            alert("Error: " + textStatus);
-					            alert("Error: " + errorThrown);
-					        }
-					    });
-
-*/
-			}
 		}
-			/*
-			else if(element[0].localName.localeCompare("a") == 0) {
-				var href = element[0].href;
-				if(element[0].href[0] === '/') {
-					console.log("replacing");
-					var re = new RegExp(window.location.origin, "g");
-					element[0].href.replace(re, href);
-				}
-
-				target = $("a[href='" + element[0].href.substring(5) + "'][text='" + element[0].text + "']");
-			}
-			else if(element[0].localName.localeCompare("p") == 0) {
-				target = $("p[innerHTML='" + element[0].innerHTML + "']");
-			}
-			if(!target) {
-				continue;
-			}
-			console.log(target);*/
-
-	};
+	};			
+	placeGIF(json);
 
 })();
+
+
+
+
+
+
+
+
+
+
+	/*
+						$.get(
+						    //delete get url here
+						    "http://hierogifics.herokuapp.com/db/remove/gif/",
+						    {   type: "GET",
+						    //List of parameters to pass to the DB
+						        data: {gif_id: data.gifs[i].gif_id more query elements} ,
+						        dataType: "json",
+						        success: function(json) {
+						            //Dont have to do anything just deletes from the DB
+						        },
+						        error: function(jqXHR, textStatus, errorThrown) {
+						        	//Displays an error if the value was not deleted
+						            alert("Error: " + jqXHR.responseText);
+						            alert("Error: " + textStatus);
+						            alert("Error: " + errorThrown);
+						        }
+						    });
+
+	*/
+				
+				/*
+				else if(element[0].localName.localeCompare("a") == 0) {
+					var href = element[0].href;
+					if(element[0].href[0] === '/') {
+						console.log("replacing");
+						var re = new RegExp(window.location.origin, "g");
+						element[0].href.replace(re, href);
+					}
+
+					target = $("a[href='" + element[0].href.substring(5) + "'][text='" + element[0].text + "']");
+				}
+				else if(element[0].localName.localeCompare("p") == 0) {
+					target = $("p[innerHTML='" + element[0].innerHTML + "']");
+				}
+				if(!target) {
+					continue;
+				}
+				console.log(target);*/
+
