802472d08626358c9ec1e8e10c2e7e60045dec12
Greyson Parrelli <greyson.parrelli@live.com>
2013-11-09 23:19:01 -0500

    All pages in same window update when you add a gif.

--- js/background_script.js
+++ js/background_script.js
@@ -107,8 +107,9 @@ function initCategories(callback) {
 
 
 function signalToolbeltRebuild() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {"refresh": true});
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+        for (var i = 0; i < tabs.length; i++)
+            chrome.tabs.sendMessage(tabs[i].id, {"refresh": true});
     });
 }
 
