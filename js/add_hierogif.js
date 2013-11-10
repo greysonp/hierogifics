f591f315eacfe5a4124351b06b324cd6812b8fa3
stephenrlouie <stephen.r.louie@gmail.com>
2013-11-09 22:46:51 -0500

    Hopefully working adder

--- js/add_hierogif.js
+++ js/add_hierogif.js
@@ -4,8 +4,30 @@ console.log("add_hierogif.js");
 	//encode JSON to URI
 	//http://hierogifics.herokuapp.com/db/create/json_encoded_page
 	//$.get("http://hierogifics.herokuapp.com/db/create/" + encoded_JSON);
+	$("img").click(function() {	
+		var gif_url = window.prompt("Please enter the url of your gif to hide it under this image element: ");
+		var userCat = window.prompt("Please categorize this gif");
+		var userID = "cheese";
+		var gifs_id = this.src;
 
-	$("img").click(function() {
-		console.log($(this).html);
+		console.log("gif_url: " + gif_url);
+		console.log("userCat: " + userCat);
+		console.log("gifs_id: " + gifs_id);
+		console.log("userID: " + userID);
+
+
+		//retrieve page information
+		var url = window.location.origin + window.location.pathname;
+			//Gets the backslashes out
+		//url = encodeURIComponent(url);
+
+		console.log("url:" + url);
+
+		$.get("http://hierogifics.herokuapp.com/db/create/" + encodeURIComponent(JSON.stringify({"url": url, "gifs": [{"id": gifs_id, "user_id": userID, "gif_url": gif_url, "category": userCat}]})), function(){
+				console.log("MIKE TOTH WAS HERE");
+
+
+
+		});
 	});
 })();
